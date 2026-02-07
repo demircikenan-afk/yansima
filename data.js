23e1fa5a7cc783b11bba7c4756f657db87fb268b
demircikenan-afk <demircikenan@gmail.com>
2026-02-07 23:12:05 +0300

    Update print statement from 'Hello' to 'Goodbye'

--- data.js
+++ data.js
@@ -46,131 +46,24 @@ const dishes = [
     { t: "Patates Salatası", s: "• Çay Saati", i: "5 kg patates, taze soğan, maydanoz, limon.", p: "Haşlanmış patatesleri doğrayıp sosla karıştırın.", g: "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?w=800" },
     { t: "Mercimek Köftesi", s: "• Marul<br>• Turşu", i: "1 kg kırmızı mercimek, 1.5 kg ince bulgur, salça, soğan.", p: "Mercimeği haşlayıp bulgurla demlendirin, harcı yoğurun.", g: "https://images.unsplash.com/photo-1512621776951-a57141f2eefd?w=800" },
     { t: "Et Sote", s: "• Pirinç Pilavı<br>• Mevsim Salata", i: "3 kg dana kuşbaşı, biber, domates, soğan.", p: "Etleri sebzelerle yüksek ateşte soteleyin.", g: "https://images.unsplash.com/photo-1603360946369-dc9bb6258143?w=800" }
-    { 
-        t: "Dondurucuda İnegöl Köfte", 
-        s: "• Salçalı Bulgur Pilavı<br>• Gavurdağı Salatası<br>• Acı Süs Biberi Turşusu", 
-        i: "2.5 kg kıyma (dana-kuzu karışık), 3 soğan suyu, 3 yk karbonat, 3 yk tuz, dondurucudan çıkarılmış hazır köfteler.", 
-        p: "Dondurucudan çıkan köfteleri çözdürmeden döküm tavada orta ateşte çevirerek pişirin. İçi sulu kalsın.", 
-        g: "https://images.unsplash.com/photo-1529006557810-274b9b2fc783?w=800" 
-    },
-    { 
-        t: "Lokanta Usulü Tavuk Haşlama", 
-        s: "• Şehriyeli Pirinç Pilavı<br>• Karışık Mevsim Salatası<br>• Sirkeli Sarımsaklı Sos", 
-        i: "5 kg tavuk but (parçalanmış), 10 patates, 10 havuç, 15 arpacık soğan, limonlu unlu terbiye.", 
-        p: "Tavukları mühürleyin, sebzeleri ekleyin. Suyla pişmeye yakın un, limon ve yumurta sarısı ile hazırlanan terbiyeyi ekleyin.", 
-        g: "https://images.unsplash.com/photo-1547592115-859fb8f2b3be?w=800" 
-    },
-    { 
-        t: "Lokanta Usulü Beyti Kebabı", 
-        s: "• Sarımsaklı Süzme Yoğurt<br>• Közlenmiş Domates ve Biber<br>• Sumaklı Soğan Salatası", 
-        i: "3 kg kıyma (kebaplık), hazır yufka, tereyağı, domates sosu, sarımsak.", 
-        p: "Köfteleri pişirin, yufkaya sarıp dilimleyin. Üzerine domates sosu ve kızgın tereyağı gezdirip yoğurtla servis edin.", 
-        g: "https://images.unsplash.com/photo-1633321702518-7feccafacdc1?w=800" 
-    },
-    { 
-        t: "Güveçte Kuşbaşılı Et", 
-        s: "• Sade Pirinç Pilavı<br>• Çoban Salata<br>• Ev Yapımı Ayran", 
-        i: "3 kg dana kuşbaşı, 10 arpacık soğan, 10 diş sarımsak, 5 kapya biber, tereyağı.", 
-        p: "Etleri sebzelerle birlikte güvece doldurun. Üzerine tereyağı koyup ağzını kapatın, fırında 2.5 saat ağır ağır pişirin.", 
-        g: "https://images.unsplash.com/photo-1603360946369-dc9bb6258143?w=800" 
-    },
-    { 
-        t: "Tavuk Döner", 
-        s: "• Patates Kızartması<br>• Lavaş Ekmeği<br>• İnce Kıyım Lahana Salatası", 
-        i: "6 kg tavuk but (ince dilimlenmiş), yoğurt, salça, bol kekik ve pul biber marinasyonu.", 
-        p: "Marine edilen tavukları sıkıca rulo yapıp dondurun. İncecik dilimleyip yüksek ateşte sacda veya tavada kızartın.", 
-        g: "https://images.unsplash.com/photo-1524338198832-7a9c18bd313c?w=800" 
-    },
-    { 
-        t: "Fırında Kuzu Kol", 
-        s: "• İç Pilav (Ciğerli Kuş Üzümlü)<br>• Ezme Salatası<br>• Közlenmiş Patlıcan", 
-        i: "2 adet kuzu kol (bütün), 10 diş sarımsak, taze biberiye, zeytinyağı, tane karabiber.", 
-        p: "Kuzu kolu sarımsak ve biberiye ile mühürleyin. Yağlı kağıda sarıp fırında 160 derecede 4 saat lokum gibi pişirin.", 
-        g: "https://images.unsplash.com/photo-1603048297172-c92544798d5a?w=800" 
-    },
-    { 
-        t: "Belen Tava", 
-        s: "• Tırnak Pidesi<br>• Mevsim Salatası<br>• Acılı Şalgam Suyu", 
-        i: "3 kg kuzu kuşbaşı, bol sarımsak, kapya biber, domates, kuyruk yağı, kekik.", 
-        p: "Tüm malzemeyi bir tepside harmanlayın, üzerine tereyağı parçaları atıp fırında suyunu salıp çekene kadar pişirin.", 
-        g: "https://images.unsplash.com/photo-1590577976322-3d234ff38235?w=800" 
-    },
-    { 
-        t: "Köfteli Hünkar Beğendi", 
-        s: "• Şehriye Çorbası<br>• Akdeniz Salatası<br>• Portakallı İrmik Helvası", 
-        i: "2 kg köftelik kıyma, 15 patlıcan, süzme yoğurt, beşamel sos malzemeleri.", 
-        p: "Patlıcanları közleyip beğendi yapın. Üzerine kızarmış lokum köfteleri domates sosuyla ekleyin.", 
-        g: "https://images.unsplash.com/photo-1604908176997-125f25cc6f3d?w=800" 
-    },
-    { 
-        t: "Sandal Köfte", 
-        s: "• Mısırlı Pirinç Pilavı<br>• Mevsim Salatası<br>• Patates Püresi", 
-        i: "2.5 kg kıyma, iç malzemesi için bezelye, havuç, mısır, üzerine kaşar peyniri.", 
-        p: "Köftelere sandal şekli verip fırınlayın. İçine sebzeli harcı ve üzerine kaşarı koyup tekrar fırına verin.", 
-        g: "https://images.unsplash.com/photo-1563805042-7684c849a135?w=800" 
-    }
-{ 
-        t: "Lokanta Usulü İzmir Köfte", 
-        s: "• Tereyağlı Şehriye Çorbası<br>• Mevsim Salatası<br>• Buz Gibi Cacık", 
-        i: "2.5 kg kıyma, 3 kg patates, 10 adet yeşil biber, 6 domates, salçalı özel sos, galeta unu, bol maydanoz.", 
-        p: "Köfteleri ve patatesleri hafifçe kızartın. Tepsiye bir köfte bir patates dizin. Üzerine salçalı, sarımsaklı suyu döküp fırında 30-40 dk pişirin.", 
-        g: "https://images.unsplash.com/photo-1529006557810-274b9b2fc783?w=800" 
-    },
-    { 
-        t: "Lokanta Usulü Kavurma", 
-        s: "• Sade Pirinç Pilavı<br>• Kuru Soğan Salatası (Sumaklı)<br>• Köpüklü Ayran", 
-        i: "4 kg dana tranç veya kuzu but, 500 gr iç yağı veya kuyruk yağı, tuz, tane karabiber.", 
-        p: "İç yağını eritip kıkırdakları alın. Etleri ekleyip yüksek ateşte mühürleyin. Kendi suyunda, kısık ateşte etler lokum gibi olana dek 2-3 saat pişirin.", 
-        g: "https://images.unsplash.com/photo-1603360946369-dc9bb6258143?w=800" 
-    },
-    { 
-        t: "Hazır Yufka İle Soslu Börek", 
-        s: "• Domatesli Şehriye Çorbası<br>• Zeytinli Akdeniz Salatası<br>• Tavşan Kanı Çay", 
-        i: "15 adet hazır yufka, 1.5 kg lor veya beyaz peynir, sosu için: 1 lt süt, 3 yumurta, 1.5 sb sıvı yağ, 1 şişe soda.", 
-        p: "Yufkaları soslayıp tepsiye dizin, arasına peyniri koyun. En üste kalan sosu döküp soda gezdirin. 180 derece fırında nar gibi kızarana kadar pişirin.", 
-        g: "https://images.unsplash.com/photo-1608483129917-c70014b91501?w=800" 
-    },
-    { 
-        t: "Patlıcanlı Gözleme", 
-        s: "• Çoban Salata<br>• Karışık Turşu<br>• Ev Yapımı Ayran", 
-        i: "20 adet hazır yufka veya el açması hamur, 3 kg patlıcan (közlenmiş veya sote), 1 kg soğan, salça, pul biber.", 
-        p: "Patlıcanları soğan ve salça ile soteleyip soğutun. Yufkanın içine koyup kapatın. Sacda veya teflon tavada tereyağı sürerek pişirin.", 
-        g: "https://images.unsplash.com/photo-1541745537411-b8046dc6d66c?w=800" 
-    },
-    { 
-        t: "Mayasız Gözleme", 
-        s: "• Domates & Salatalık Söğüş<br>• Beyaz Peynir ve Zeytin<br>• Taze Demlenmiş Çay", 
-        i: "3 kg un, su, tuz (iç harcı için peynirli veya patatesli isteğe bağlı).", 
-        p: "Un, su ve tuzla yumuşak bir hamur yoğurup dinlendirin. Bezeleri ince açıp iç harcı ekleyin ve sacda pişirip yağlayın.", 
-        g: "https://images.unsplash.com/photo-1541745537411-b8046dc6d66c?w=800" 
-    },
-    { 
-        t: "Şehriyeli Sulu Köfte", 
-        s: "• Mevsim Salatası<br>• Karışık Turşu<br>• Vişne Kompostosu", 
-        i: "2 kg kıyma, 2 sb pirinç veya ince bulgur, 5 patates, 3 havuç, 1.5 sb arpa şehriye, terbiyesi için limon ve yumurta sarısı.", 
-        p: "Köfteleri yuvarlayıp unlayın. Kaynayan sebzeli suya şehriyeleri ve köfteleri ekleyin. Pişmeye yakın terbiyesini verip bir taşım kaynatın.", 
-        g: "https://images.unsplash.com/photo-1547592115-859fb8f2b3be?w=800" 
-    },
-    { 
-        t: "Fırında Köfte Patates", 
-        s: "• Domatesli Şehriyeli Bulgur Pilavı<br>• Cacık<br>• Mevsim Salatası", 
-        i: "2.5 kg kıyma, 4 kg patates, 10 adet sivri biber, 6 domates, salçalı su.", 
-        p: "Köfteleri hazırlayıp patateslerle birlikte fırın tepsisine dizin. Üzerine salçalı ve kekikli su döküp 200 derecede pişirin.", 
-        g: "https://images.unsplash.com/photo-1529006557810-274b9b2fc783?w=800" 
-    },
-    { 
-        t: "Soslu Köfte", 
-        s: "• Sade Pirinç Pilavı<br>• Havuç Tarator<br>• Naneli Ayran", 
-        i: "2.5 kg kıyma, bol sarımsaklı domates sosu, 1 kg kapya biber, kekik, fesleğen.", 
-        p: "Köfteleri kızartın. Ayrı bir yerde bol domates ve sarımsakla yoğun bir sos hazırlayın. Köfteleri sosla buluşturup fırında 10 dk daha pişirin.", 
-        g: "https://images.unsplash.com/photo-1529006557810-274b9b2fc783?w=800" 
-    },
-    { 
-        t: "Domates Soslu Köfte", 
-        s: "• Spagetti Makarna<br>• Mevsim Salatası<br>• Limonata", 
-        i: "2 kg kıyma, 2 kg olgun domates (rendelenmiş), 5 diş sarımsak, tereyağı.", 
-        p: "Köfteleri yuvarlayıp pişirin. Rendelenmiş domatesleri sarımsakla pişirip sos yapın. Köfteleri içine atıp 5 dk çektirin.", 
-        g: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?w=800" 
-    }
+    { t: "İnegöl Köfte (Lokanta Usulü)", s: "• Salçalı Bulgur Pilavı<br>• Mevsim Salatası<br>• Buz Gibi Ayran", i: "2.5 kg kıyma, karbonat, soğan suyu, tuz.", p: "Yoğurup dinlendirin, parmak şekli verip kızartın.", g: "https://images.unsplash.com/photo-1529006557810-274b9b2fc783?w=800" },
+    { t: "Tavuk Haşlama", s: "• Pirinç Pilavı<br>• Turşu<br>• Sirkeli Sos", i: "5 kg tavuk, patates, havuç, unlu terbiye.", p: "Tavukları sebzelerle haşlayıp terbiyesini ekleyin.", g: "https://images.unsplash.com/photo-1547592115-859fb8f2b3be?w=800" },
+    { t: "Lokanta Usulü Beyti Kebabı", s: "• Süzme Yoğurt<br>• Köz Biber<br>• Sumaklı Soğan", i: "3 kg kıyma, yufka, sosluk domates, tereyağı.", p: "Köfteleri yufkaya sarın, üzerine sos döküp fırınlayın.", g: "https://images.unsplash.com/photo-1633321702518-7feccafacdc1?w=800" },
+    { t: "Güveçte Kuşbaşılı Et", s: "• Pilav<br>• Çoban Salata<br>• Ayran", i: "3 kg et, arpacık soğan, sarımsak, salça.", p: "Güvece dizip ağır ateşte fırında pişirin.", g: "https://images.unsplash.com/photo-1603360946369-dc9bb6258143?w=800" },
+    { t: "Tavuk Döner", s: "• Lavaş<br>• Patates Kızartması<br>• Şalgam", i: "6 kg tavuk, özel marinasyon sosu.", p: "Dondurup ince dilimleyin ve sacda pişirin.", g: "https://images.unsplash.com/photo-1524338198832-7a9c18bd313c?w=800" },
+    { t: "Fırında Kuzu Kol", s: "• İç Pilav<br>• Ezme<br>• Köz Patlıcan", i: "Bütün kuzu kol, sarımsak, biberiye.", p: "Fırında 4 saat ağır ağır pişirin.", g: "https://images.unsplash.com/photo-1603048297172-c92544798d5a?w=800" },
+    { t: "Lokanta Usulü İzmir Köfte", s: "• Mercimek Çorbası<br>• Salata<br>• Cacık", i: "2.5 kg kıyma, patates, domates sosu.", p: "Kızartıp sosla fırınlayın.", g: "https://images.unsplash.com/photo-1529006557810-274b9b2fc783?w=800" },
+    { t: "Lokanta Usulü Kavurma", s: "• Pilav<br>• Soğan Salatası<br>• Ayran", i: "4 kg et, iç yağı, tuz.", p: "Kendi suyunda lokum gibi pişirin.", g: "https://images.unsplash.com/photo-1603360946369-dc9bb6258143?w=800" },
+    { t: "Hazır Yufka Soslu Börek", s: "• Çay<br>• Peynir<br>• Zeytin", i: "Yufka, peynir, sütlü özel sos.", p: "Yufkaları soslayıp fırınlayın.", g: "https://images.unsplash.com/photo-1608483129917-c70014b91501?w=800" },
+    { t: "Patlıcanlı Gözleme", s: "• Ayran<br>• Turşu<br>• Söğüş", i: "Yufka, patlıcanlı iç harç.", p: "Sacda pişirip tereyağı ile yağlayın.", g: "https://images.unsplash.com/photo-1541745537411-b8046dc6d66c?w=800" },
+    { t: "Mayasız Gözleme", s: "• Çay<br>• Reçel<br>• Peynir", i: "Un, su, tuz.", p: "Hamuru açıp içine peynir koyup pişirin.", g: "https://images.unsplash.com/photo-1541745537411-b8046dc6d66c?w=800" },
+    { t: "Köfteli Hünkar Beğendi", s: "• Çorba<br>• Salata<br>• Helva", i: "Köfte, patlıcan, beşamel sos.", p: "Beğendi üzerine köfteleri dizin.", g: "https://images.unsplash.com/photo-1604908176997-125f25cc6f3d?w=800" },
+    { t: "Belen Tava", s: "• Pide<br>• Salata<br>• Ayran", i: "Kuzu eti, bol sarımsak, biber.", p: "Tüm malzemeyi harmanlayıp fırınlayın.", g: "https://images.unsplash.com/photo-1590577976322-3d234ff38235?w=800" },
+    { t: "Şehriyeli Sulu Köfte", s: "• Turşu<br>• Salata<br>• Komposto", i: "Kıyma, patates, arpa şehriye.", p: "Tencerede terbiyeli pişirin.", g: "https://images.unsplash.com/photo-1547592115-859fb8f2b3be?w=800" },
+    { t: "Fırında Köfte Patates", s: "• Bulgur Pilavı<br>• Cacık<br>• Salata", i: "Kıyma, patates, salçalı su.", p: "Tepsiye dizip fırınlayın.", g: "https://images.unsplash.com/photo-1529006557810-274b9b2fc783?w=800" },
+    { t: "Soslu Köfte", s: "• Pilav<br>• Tarator<br>• Ayran", i: "Kıyma, yoğun domates sosu.", p: "Kızartıp sosla fırınlayın.", g: "https://images.unsplash.com/photo-1529006557810-274b9b2fc783?w=800" },
+    { t: "Domates Soslu Köfte", s: "• Makarna<br>• Salata<br>• Limonata", i: "Kıyma, taze domates rendesi.", p: "Sosta pişirin.", g: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?w=800" },
+    { t: "Sandal Köfte", s: "• Pilav<br>• Salata<br>• Püre", i: "Kıyma, bezelye, kaşar.", p: "İçini doldurup fırınlayın.", g: "https://images.unsplash.com/photo-1563805042-7684c849a135?w=800" }
 ];
+
 
